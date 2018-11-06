52e6df58e028dfaa3b90303fb26cef82bbbc76b3
freePyDeveloper <freePyDeveloper@gmail.com>
2018-11-06 10:22:52 +0330

    add eventsBus

--- consensus/FRONTEND/consensus-ui/src/main.js
+++ consensus/FRONTEND/consensus-ui/src/main.js
@@ -1,23 +1,15 @@
 import Vue from "vue";
-import axios from "axios";
-
+import EventBus from "./event-bus";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
-Vue.prototype.$eventsBus = new Vue();
+Vue.prototype.$eventsBus = EventBus;
 
 /****************************************
  ****************** App *****************
  ****************************************/
-
-axios.defaults.baseURL = "/api/v1";
-axios.defaults.xsrfCookieName = "csrftoken";
-axios.defaults.xsrfHeaderName = "X-CSRFToken";
-axios.defaults.withCredentials = true; // allow to pass cookie in cross origin
-
-Vue.prototype.$http = axios;
 
 new Vue({
   router,
