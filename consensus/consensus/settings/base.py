68cc8ee59f1a5433645403654dce0b71d4cbd3a7
pprolancer <pprolancer@gmail.com>
2018-11-07 09:05:13 -0800

    - updated the setting for external config

--- consensus/consensus/settings/base.py
+++ consensus/consensus/settings/base.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-EXTERNAL_CONFIG_PATH = os.path.join(BASE_DIR, 'consensus', 'settings', 'external_config.py')
+EXTERNAL_CONFIG_PATH = '/opt/webapps/consensus/etc/external_config.py'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
