526f9fe75a3b1869d6c43634ea863e82673355c3
freePyDeveloper <freePyDeveloper@gmail.com>
2018-11-14 08:25:27 +0330

    use vuetableBoostrapMixin

--- consensus/FRONTEND/consensus-ui/src/components/school/Home.vue
+++ consensus/FRONTEND/consensus-ui/src/components/school/Home.vue
@@ -42,10 +42,11 @@
 import UtilMixin from "@/mixins/UtilMixin";
 import Vuetable from "vuetable-2/src/components/Vuetable";
 import VuetablePagination from "vuetable-2/src/components/VuetablePagination";
+import VuetableBootstrapMixin from "../../mixins/VuetableBootstrapMixin";
 
 export default {
   name: "Home",
-  mixins: [UtilMixin],
+  mixins: [UtilMixin, VuetableBootstrapMixin],
   components: {
     Vuetable,
     VuetablePagination
@@ -68,38 +69,9 @@ export default {
           dataClass: "text-left"
         }
       ],
-      css: {
-        table: {
-          tableClass: "table table-striped table-bordered",
-          ascendingIcon: "glyphicon glyphicon-chevron-up",
-          descendingIcon: "glyphicon glyphicon-chevron-down",
-          handleIcon: "glyphicon glyphicon-menu-hamburger"
-        },
-        pagination: {
-          infoClass: "pull-left",
-          wrapperClass: "vuetable-pagination pull-right",
-          activeClass: "btn-primary",
-          disabledClass: "disabled",
-          pageClass: "btn btn-border",
-          linkClass: "btn btn-border",
-          icons: {
-            first: "",
-            prev: "",
-            next: "",
-            last: ""
-          }
-        }
-      },
       schools: []
     };
   },
-  methods: {
-    onPaginationData(paginationData) {
-      this.$refs.pagination.setPaginationData(paginationData);
-    },
-    onChangePage(page) {
-      this.$refs.vuetable.changePage(page);
-    }
-  }
+  methods: {}
 };
 </script>
