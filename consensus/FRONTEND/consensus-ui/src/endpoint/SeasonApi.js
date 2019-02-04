604b21355ee0f26afb92d0ca43cdd0eca3097cb5
freePyDeveloper <freePyDeveloper@gmail.com>
2019-02-04 17:13:48 +0330

    add migration files

--- consensus/FRONTEND/consensus-ui/src/endpoint/SeasonApi.js
+++ consensus/FRONTEND/consensus-ui/src/endpoint/SeasonApi.js
@@ -2,15 +2,15 @@ import Api from "@/endpoint/Api";
 
 export default {
   getBySchoolId(schoolId) {
-    return Api.get(`school\\${schoolId}\\season`);
+    return Api.get(`school/${schoolId}/season`);
   },
   add(schoolId, season) {
-    return Api.post(`school\\${schoolId}\\season`, season);
+    return Api.post(`school/${schoolId}/season`, season);
   },
   put(schoolId, season) {
-    return Api.put(`school\\${schoolId}\\season\\${season.id}`, season);
+    return Api.put(`school/${schoolId}/season/${season.id}`, season);
   },
   delete(schoolId, season) {
-    return Api.delete(`school\\${schoolId}\\season\\${season.id}`);
+    return Api.delete(`school/${schoolId}/season/${season.id}`);
   }
 };
--- consensus/FRONTEND/consensus-ui/src/endpoint/StaffApi.js
+++ consensus/FRONTEND/consensus-ui/src/endpoint/StaffApi.js
@@ -2,15 +2,15 @@ import Api from "@/endpoint/Api";
 
 export default {
   getBySchoolId(schoolId) {
-    return Api.get(`school\\${schoolId}\\staff`);
+    return Api.get(`school/${schoolId}/staff`);
   },
   add(schoolId, staff) {
-    return Api.post(`school\\${schoolId}\\staff`, staff);
+    return Api.post(`school/${schoolId}/staff`, staff);
   },
   put(schoolId, staff) {
-    return Api.put(`school\\${schoolId}\\staff\\${staff.id}`, staff);
+    return Api.put(`school/${schoolId}/staff/${staff.id}`, staff);
   },
   delete(schoolId, staff) {
-    return Api.delete(`school\\${schoolId}\\staff\\${staff.id}`);
+    return Api.delete(`school/${schoolId}/staff/${staff.id}`);
   }
 };
