8648aee4ad06d6ba7c0a5fa23bb4ae6d4b499e16
freePyDeveloper <freePyDeveloper@gmail.com>
2019-02-20 17:24:45 +0330

    use build in permission

--- consensus/apps/school/admin.py
+++ consensus/apps/school/admin.py
@@ -1,16 +1,16 @@
-from admin_view_permission.admin import AdminViewPermissionAdminSite
 from django.contrib import admin
 
+from reversion.admin import VersionAdmin
 from apps.school.models import School, Application, Score, Season, Staff
 
 
-class SchoolInstanceAdmin(AdminViewPermissionAdminSite):
+class SchoolInstanceAdmin(VersionAdmin):
     list_display = ('full_name', 'phone_number', 'address', 'country', 'country', 'state', 'city', 'zip_code')
     list_filter = ('full_name', 'phone_number', 'address', 'country', 'country', 'state', 'city', 'zip_code')
 
 
-admin.site.register(School)
-admin.site.register(Application)
-admin.site.register(Score)
-admin.site.register(Season)
-admin.site.register(Staff)
+admin.site.register(School, SchoolInstanceAdmin)
+admin.site.register(Application, VersionAdmin)
+admin.site.register(Score, VersionAdmin)
+admin.site.register(Season, VersionAdmin)
+admin.site.register(Staff, VersionAdmin)
